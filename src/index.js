fa34d0423f291c568c2bc7ec2d111f13da04e3e1
Nimrod <n.wainberg@gmail.com>
2022-03-08 00:59:33 +0200

    store-redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import store from "./store/store";
+import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createStore } from "redux";
+import AllReducers from ".././reducers";
+
+const store = createStore(
+  AllReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);

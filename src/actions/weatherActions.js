857177f7b7e20bdcaf76056de63c4c69496fdd66
Nimrod <n.wainberg@gmail.com>
2022-03-11 03:23:06 +0200

    [Done]- weather reduce, actions

---
+++ src/actions/weatherActions.js
@@ -0,0 +1,27 @@
+export const addWeather = (weather) => {
+  return {
+    type: "ADD_WEATHER",
+    payload: weather,
+  };
+};
+
+export const addFavorite = (weather) => {
+  return {
+    type: "ADD_FAVORITE",
+    payload: weather,
+  };
+};
+
+export const setCurrentWeatherData = (weatherData) => {
+  return {
+    type: "SET_CURRENT_WEATHER_DATA",
+    payload: weatherData[0],
+  };
+};
+
+export const setCurrentWeatherName = (weatherName) => {
+  return {
+    type: "SET_CURRENT_WEATHER_NAME",
+    payload: weatherName,
+  };
+};
--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -1,8 +1,32 @@
 const weatherRducer = (
-  state = { weathers: [], favorites: [], currentWeather: null },
+  state = { weathers: [], favorites: [], currentWeatherData: {} },
   action
 ) => {
   switch (action.type) {
+    case "SET_CURRENT_WEATHER_DATA": {
+      console.log(`action.payload: ${JSON.stringify(action.payload)}`);
+      return {
+        ...state,
+        currentWeatherData: {
+          weatherText: action.payload.WeatherText,
+          temperature: action.payload.Temperature.Metric.Value,
+        },
+      };
+    }
+
+    case "SET_CURRENT_WEATHER_NAME": {
+      // return {
+      //   ...state,
+      //   currentWeatherData: {
+      //     cityName: action.payload,
+      //   },
+      // };
+      return {
+        ...state,
+        cityName: action.payload,
+      };
+    }
+
     default:
       return state;
   }

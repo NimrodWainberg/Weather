447b52044b7b6416e85f242d6cc7f65ced78d8c6
Nimrod <n.wainberg@gmail.com>
2022-03-11 03:23:40 +0200

    change pages-> home

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -9,6 +9,7 @@ import { getAutoCompleteApi } from "../../mock/mock";
 const Layout = () => {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
+  const [autoCompleteOptions, setAutoCompleteOptions] = useState([]);
 
   // const notify = () => toast("Wow so easy!");
 
@@ -19,113 +20,13 @@ const Layout = () => {
     navigate(`/${event.target.value}`);
   };
 
-  const weatherApi = {
-    baseKey: "7j7jodfnbvOacTr6XdFeOJ7XmqQhTSMg",
-    autoCompleteKey:
-      "https://dataservice.accuweather.com/locations/v1/cities/autocomplete",
-    currentWeatherKey:
-      "https://dataservice.accuweather.com/currentconditions/v1/",
-  };
-
-  const getWeatherDetails = async (city) => {
-    setQuery(`${city}?apikey=${weatherApi.baseKey}`);
-
-    const res = await fetch(weatherApi.currentWeatherKey + query);
-    const data = await res.json();
-
-    return data;
-  };
-
-  const getWeatherAutoComplete = async (locationPartialName) => {
-    setQuery(`?apikey=${weatherApi.baseKey}&q=${locationPartialName}`);
-
-    console.log(`query:${query}`);
-
-    try {
-      console.log(weatherApi.autoCompleteKey + query);
-      //const res = await fetch(weatherApi.autoCompleteKey + query);
-
-      // const data = await res.json();
-      const data = getAutoCompleteApi();
-      return data;
-    } catch (err) {
-      toast.error({ id: "error" }, "auto complete failed");
-    }
-  };
-
-  const handleChange = async (event) => {
-    // notify(event)
-    // <ToastContainer/>
-    console.log(event.target.value);
-    let options = await getWeatherAutoComplete(event.target.value);
-    //let currentWeather = getWeatherDetails(event.target.value);
-    console.log(options);
-  };
-
-  const search = (event) => {
-    if (event.key === "Enter") {
-    }
-  };
-
-  const dateBuilder = (date) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    let day = days[date.getDay()];
-    let day_number = date.getDate();
-    let month = months[date.getMonth()];
-    let year = date.getFullYear();
-
-    return `${day} ${day_number} ${month} ${year}`;
-  };
-
   return (
-    <div className="app">
+    <div className="">
       <div className="dashboard">
         <h1>Dashboard</h1>
-        <DropDown />
         <GroupButtons onClick={(e) => handleClick(e)}></GroupButtons>
       </div>
       <main>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search"
-            onChange={handleChange}
-            onKeyPress={search}
-          />
-        </div>
-        <div className="location-box">
-          <div className="location">Tel Aviv City</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temperature">15°c</div>
-          <div className="weather">Sunny</div>
-        </div>
-        {/* <button onClick={notify}>Notify!</button> */}
         <ToastContainer />
         <Outlet />
       </main>

9182236fa694ff74eb8cecdd76c51999a7fce377
Nimrod <n.wainberg@gmail.com>
2022-03-11 03:24:02 +0200

    [Done]- combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import weather from "./weather";
+import autoComplete from "./autoComplete";
 import { combineReducers } from "redux";
 
 const allReducers = combineReducers({
   weather,
+  autoComplete,
 });
 
 export default allReducers;

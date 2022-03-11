7431f8590fdc4e40c94e3b8a20dcd48aeafdee5d
Nimrod <n.wainberg@gmail.com>
2022-03-11 03:23:20 +0200

    api mocks

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -152,3 +152,99 @@ export function getAutoCompleteApi() {
     },
   ];
 }
+
+export function getCitySearch() {
+  return [
+    {
+      Version: 1,
+      Key: "215854",
+      Type: "City",
+      Rank: 31,
+      LocalizedName: "Tel Aviv",
+      EnglishName: "Tel Aviv",
+      PrimaryPostalCode: "",
+      Region: {
+        ID: "MEA",
+        LocalizedName: "Middle East",
+        EnglishName: "Middle East",
+      },
+      Country: {
+        ID: "IL",
+        LocalizedName: "Israel",
+        EnglishName: "Israel",
+      },
+      AdministrativeArea: {
+        ID: "TA",
+        LocalizedName: "Tel Aviv",
+        EnglishName: "Tel Aviv",
+        Level: 1,
+        LocalizedType: "District",
+        EnglishType: "District",
+        CountryID: "IL",
+      },
+      TimeZone: {
+        Code: "IST",
+        Name: "Asia/Jerusalem",
+        GmtOffset: 2,
+        IsDaylightSaving: false,
+        NextOffsetChange: "2022-03-25T00:00:00Z",
+      },
+      GeoPosition: {
+        Latitude: 32.045,
+        Longitude: 34.77,
+        Elevation: {
+          Metric: {
+            Value: 34,
+            Unit: "m",
+            UnitType: 5,
+          },
+          Imperial: {
+            Value: 111,
+            Unit: "ft",
+            UnitType: 0,
+          },
+        },
+      },
+      IsAlias: false,
+      SupplementalAdminAreas: [],
+      DataSets: [
+        "AirQualityCurrentConditions",
+        "AirQualityForecasts",
+        "Alerts",
+        "DailyPollenForecast",
+        "ForecastConfidence",
+        "FutureRadar",
+        "MinuteCast",
+      ],
+    },
+  ];
+}
+
+export function getCurrentCityWeather() {
+  return [
+    {
+      LocalObservationDateTime: "2022-03-10T23:58:00+02:00",
+      EpochTime: 1646949480,
+      WeatherText: "Partly cloudy",
+      WeatherIcon: 35,
+      HasPrecipitation: false,
+      PrecipitationType: null,
+      IsDayTime: false,
+      Temperature: {
+        Metric: {
+          Value: 14.4,
+          Unit: "C",
+          UnitType: 17,
+        },
+        Imperial: {
+          Value: 58,
+          Unit: "F",
+          UnitType: 18,
+        },
+      },
+      MobileLink:
+        "http://www.accuweather.com/en/il/tel-aviv/215854/current-weather/215854?lang=en-us",
+      Link: "http://www.accuweather.com/en/il/tel-aviv/215854/current-weather/215854?lang=en-us",
+    },
+  ];
+}
